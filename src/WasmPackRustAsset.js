3f7a22d864309e617b9ac2d67a4e741780f791ab
Matt Hayes <matt@mysterycommand.com>
2019-04-11 10:28:56 -0400

    refactor(plugin): more poking around

--- src/WasmPackRustAsset.js
+++ src/WasmPackRustAsset.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const childProcess = require('child_process');
+const { execFile, spawn } = require('child_process');
 const { promisify } = require('util');
 
 const fs = require('@parcel/fs');
@@ -10,7 +10,49 @@ const commandExists = require('command-exists');
 const RustAsset = require('parcel-bundler/src/assets/RustAsset');
 const config = require('parcel-bundler/src/utils/config');
 
-const exec = promisify(childProcess.execFile);
+const exec = promisify(execFile);
+function proc(bin, args, opts) {
+  return new Promise((resolve, reject) => {
+    const p = spawn(bin, args, opts);
+
+    let stdout = '';
+    let stdoutLine = '';
+    let stderr = '';
+    let stderrLine = '';
+
+    p.stdout.on('data', d => {
+      stdoutLine += d;
+
+      if (stdoutLine.includes('\n')) {
+        stdout += stdoutLine;
+        const lines = stdoutLine.split('\n');
+        lines.slice(0, -1).forEach(line => logger.log(line));
+        stdoutLine = lines.slice(-1)[0];
+      }
+    });
+
+    p.stderr.on('data', d => {
+      stderrLine += d;
+
+      if (stderrLine.includes('\n')) {
+        stderr += stderrLine;
+        const lines = stderrLine.split('\n');
+        lines.slice(0, -1).forEach(line => logger.log(line));
+        stderrLine = lines.slice(-1)[0];
+      }
+    });
+
+    p.on('close', code => {
+      if (code === 0) {
+        resolve(stdout);
+      } else {
+        reject(stderr);
+      }
+    });
+
+    p.on('error', reject);
+  });
+}
 
 /**
  * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/assets/RustAsset.js#L13-L14
@@ -33,10 +75,11 @@ class WasmPackRustAsset extends RustAsset {
      */
     await super.installRust();
 
-    await this.cargoInstall('wasm-pack');
-    await this.cargoInstall('wasm-bindgen', 'wasm-bindgen-cli');
-
     const { cargoConfig, cargoDir, isMainFile } = await this.getCargoConfig();
+    await this.cargoInstall('wasm-pack', { cwd: cargoDir });
+    await this.cargoInstall('wasm-bindgen', 'wasm-bindgen-cli', {
+      cwd: cargoDir,
+    });
 
     if (isMainFile) {
       await this.ensureCargoConfig(cargoConfig, cargoDir);
@@ -50,11 +93,12 @@ class WasmPackRustAsset extends RustAsset {
     }
   }
 
-  async cargoInstall(cmd, dep) {
-    dep || (dep = cmd);
+  async cargoInstall(cmd, bin, opts) {
+    opts || (opts = bin);
+    bin || bin === opts || (bin = cmd);
     logger.log(`installing ${cmd}`);
 
-    if (installed[dep]) {
+    if (installed[bin]) {
       logger.log(`${cmd} already installed, skipping`);
       return;
     }
@@ -62,26 +106,19 @@ class WasmPackRustAsset extends RustAsset {
     await commandExists(cmd)
       .then(() => {
         logger.log(`${cmd} already installed, skipping`);
-        installed[dep] = true;
+        installed[bin] = true;
       })
-      .catch(
-        () =>
-          new Promise((resolve, reject) => {
-            logger.log(`${cmd} not installed, trying \`cargo install ${dep}\``);
-
-            exec('cargo', ['install', dep], (err, stdout) => {
-              if (err) {
-                installed[dep] = false;
-                logger.err(`something went wrong, ${cmd} not installed`);
-                err.split('\n').forEach(line => logger.err(line));
-                reject(err);
-              }
-
-              installed[dep] = true;
-              logger.log(`${cmd} installed successfully!`);
-              stdout.split('\n').forEach(line => logger.log(line));
-              resolve(stdout);
-            });
+      .catch(() =>
+        proc('cargo', ['--verbose', 'install', bin])
+          .then((/* stdout */) => {
+            installed[bin] = true;
+            logger.log(`${cmd} installed successfully!`);
+            // stdout.split('\n').forEach(line => logger.log(line));
+          })
+          .catch((/* stderr */) => {
+            installed[bin] = false;
+            logger.error(`something went wrong, ${cmd} not installed`);
+            // stderr.split('\n').forEach(line => logger.error(line));
           }),
       );
   }
@@ -146,12 +183,14 @@ class WasmPackRustAsset extends RustAsset {
 
   async wasmPackBuild(cargoConfig, cargoDir) {
     const args = [
+      '--verbose',
       'build',
       ...(installed['wasm-bindgen-cli'] ? ['-m', 'no-install'] : []),
+      '--target=web',
     ];
 
     logger.log(`running \`wasm-pack ${args.join(' ')}\``);
-    await exec('wasm-pack', args, { cwd: cargoDir });
+    await proc('wasm-pack', args, { cwd: cargoDir });
 
     this.outDir = path.join(cargoDir, 'pkg');
     this.rustName = cargoConfig.package.name.replace(/-/g, '_');
@@ -164,7 +203,7 @@ class WasmPackRustAsset extends RustAsset {
     // Get output file paths
     const { stdout } = await exec(
       'cargo',
-      ['metadata', '--format-version', '1'],
+      ['--verbose', 'metadata', '--format-version', '1'],
       {
         cwd: cargoDir,
       },
@@ -175,39 +214,46 @@ class WasmPackRustAsset extends RustAsset {
   }
 
   async generate() {
-    const loader = await this.getLoader(
-      path.relative('./src', this.wasmPath),
-      path.relative('./src', this.bindgenPath),
-    );
+    const bindgen = (await fs.readFile(this.bindgenPath)).toString();
+
+    const exportFunction = /export function (\w+)/g;
+    const importNames = [];
+    let a;
+    while ((a = exportFunction.exec(bindgen)) !== null) {
+      importNames.push(a[1]);
+    }
+
+    const basename = path.basename(this.bindgenPath, '.js');
+    const relpath = path.relative('./src', path.dirname(this.bindgenPath));
+    const loader = `\
+import init from '${path.join(relpath, basename)}';
+let wasm;
+
+(async function load() {
+  wasm = await init('./${path.relative(this.outDir, this.wasmPath)}');
+})();
+
+${importNames.map(name => `export const ${name} = wasm.${name};`).join('\n')}
+`;
     // await fs.writeFile(path.join(this.outDir, 'loader.js'), loader);
 
-    const bindgen = (await fs.readFile(this.bindgenPath))
-      .toString()
-      .replace(`import * as wasm from './${this.rustName}_bg';`, 'const wasm;');
-    await fs.writeFile(this.bindgenPath, bindgen);
-
-    return [
-      {
-        type: 'js',
+    return {
+      /**
+       * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/assets/RustAsset.js#L189-L190
+       */
+      wasm: {
+        path: this.wasmPath, // pass output path to RawPackager
+        mtime: Date.now(), // force re-bundling since otherwise the hash would never change
+      },
+      js: {
         path: this.bindgenPath,
+        mtime: Date.now(),
       },
-      {
-        type: 'js',
-        value: loader,
-      },
-    ];
-  }
-
-  async getLoader(wasmPath, bindgenPath) {
-    const loaderTemplate =
-      this.options.target === 'node'
-        ? await ''
-        : (await fs.readFile(
-            require.resolve('./loaders/browser.js'),
-          )).toString();
-    return loaderTemplate
-      .replace(/WASM_PATH/g, `${wasmPath}`)
-      .replace(/RUST_NAME/g, `${bindgenPath}`);
+      // {
+      //   type: 'js',
+      //   value: loader,
+      // },
+    };
   }
 }
 
