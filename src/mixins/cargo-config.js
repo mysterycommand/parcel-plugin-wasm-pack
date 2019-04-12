75b44066c1d07c68d4696dc4c08e99efaf07e17b
Matt Hayes <matt@mysterycommand.com>
2019-04-12 10:42:28 -0400

    refactor(plugin): these need a 'this' so they get mixed in

---
+++ src/mixins/cargo-config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+const fs = require('@parcel/fs');
+const toml = require('@iarna/toml');
+
+const config = require('parcel-bundler/src/utils/config');
+
+const MAIN_FILES = ['src/lib.rs', 'src/main.rs'];
+
+/**
+ * pulled out from Parcel's RustAsset class:
+ * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/assets/RustAsset.js#L40-L55
+ *
+ * @memberof WasmPackRustAsset
+ */
+async function getCargoConfig() {
+  // See if there is a Cargo config in the project
+  let cargoConfig = await this.getConfig(['Cargo.toml']);
+  let cargoDir;
+  let isMainFile = false;
+
+  if (cargoConfig) {
+    const mainFiles = MAIN_FILES.slice();
+    if (cargoConfig.lib && cargoConfig.lib.path) {
+      mainFiles.push(cargoConfig.lib.path);
+    }
+
+    cargoDir = path.dirname(await config.resolve(this.name, ['Cargo.toml']));
+    isMainFile = mainFiles.some(
+      file => path.join(cargoDir, file) === this.name,
+    );
+  }
+
+  this.cargoConfig = cargoConfig;
+  this.cargoDir = cargoDir;
+  this.isMainFile = isMainFile;
+}
+
+/**
+ * pulled out from Parcel's RustAsset class:
+ * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/assets/RustAsset.js#L108-L123
+ *
+ * @memberof WasmPackRustAsset
+ */
+async function ensureCargoConfig() {
+  const { cargoConfig, cargoDir } = this;
+
+  // Ensure the cargo config has cdylib as the crate-type
+  if (!cargoConfig.lib) {
+    cargoConfig.lib = {};
+  }
+
+  if (!Array.isArray(cargoConfig.lib['crate-type'])) {
+    cargoConfig.lib['crate-type'] = [];
+  }
+
+  if (!cargoConfig.lib['crate-type'].includes('cdylib')) {
+    cargoConfig.lib['crate-type'].push('cdylib');
+
+    await fs.writeFile(
+      path.join(cargoDir, 'Cargo.toml'),
+      toml.stringify(cargoConfig),
+    );
+  }
+}
+
+module.exports = { getCargoConfig, ensureCargoConfig };
