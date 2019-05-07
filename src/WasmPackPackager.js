375d87c3c6bb7b160fb034bb23e2c5ebbe92c54a
Matt Hayes <matt@mysterycommand.com>
2019-05-06 21:09:12 -0400

    refactor(WasmPackPackager): reorganize code for clarity, add notes

--- src/WasmPackPackager.js
+++ src/WasmPackPackager.js
@@ -2,33 +2,57 @@ const path = require('path');
 
 const JSPackager = require('parcel-bundler/src/packagers/JSPackager');
 
+const moduleTpl = (wasmName, wasmId, entryName) => `\
+require('${wasmName}')
+  .then(wasm => {
+    // replace the entry in the cache with the loaded wasm module
+    module.bundle.cache['${wasmId}'] = null;
+    module.bundle.register('${wasmId}', wasm);
+  })
+  .then(() => {
+    require('${entryName}');
+  });
+`;
+
 class WasmPackPackager extends JSPackager {
   async writeBundleLoaders() {
-    const assets = Array.from(this.bundle.assets.values());
+    const assets = [...this.bundle.assets.values()];
     const wasmAsset = assets.find(({ isWasm }) => isWasm);
 
     if (!wasmAsset) {
       return await super.writeBundleLoaders();
     }
 
-    const mod = `
-require('./lib.rs')
-  .then(wasm => {
-    module.bundle.register('hi', wasm);
-  })
-  .then(() => {
-    require('./entry.ts');
-  });
-`;
-    const deps = { './lib.rs': 'jBO1', './entry.ts': 'QAnz' };
+    const { entryAsset } = this.bundle;
+
+    const wasmName = path.relative(
+      path.dirname(entryAsset.name),
+      wasmAsset.name,
+    );
+    const entryName = path.relative(
+      path.dirname(entryAsset.name),
+      entryAsset.name,
+    );
+
+    /**
+     * make sure we don't add the original entry to the final bundle's entries
+     * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/packagers/JSPackager.js#L225-L227
+     */
+    this.externalModules.add('');
+
+    const mod = moduleTpl(wasmName, wasmAsset.id, entryName);
+    const deps = {
+      [wasmName]: wasmAsset.id,
+      [entryName]: entryAsset.id,
+    };
+
+    /**
+     * asset ids normally start at 1, so this should be safe
+     * @see: https://github.com/parcel-bundler/parcel/blob/master/packages/core/parcel-bundler/src/packagers/JSPackager.js#L204
+     */
     await this.writeModule(0, mod, deps);
     return true;
   }
-
-  // async end() {
-  //   await super.end();
-  //   debugger;
-  // }
 }
 
 module.exports = WasmPackPackager;
