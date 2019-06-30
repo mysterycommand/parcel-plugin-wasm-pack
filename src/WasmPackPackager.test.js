2b266b2b88c16fd4b32264acb886848f1ab9a683
Matt Hayes <matt@mysterycommand.com>
2019-06-30 15:16:48 -0400

    test(WasmPackPackager): first pass at mocking/spying on the WasmPackPackager

--- src/WasmPackPackager.test.js
+++ src/WasmPackPackager.test.js
@@ -1,7 +1,27 @@
+const JSPackager = require('parcel-bundler/src/packagers/JSPackager');
+
 const WasmPackPackager = require('./WasmPackPackager');
 
 describe('WasmPackPackager', () => {
-  it('should exist', () => {
-    expect(WasmPackPackager).toBeDefined();
+  it('should write a loader if this package has wasm assets', async () => {
+    const writeBundleLoadersSpy = jest.spyOn(
+      JSPackager.prototype,
+      'writeBundleLoaders',
+    );
+
+    const packager = new WasmPackPackager(
+      {
+        assets: new Set(),
+        entryAsset: { name: './index.js' },
+        name: './index.js',
+      },
+      { options: { hmr: false } },
+    );
+
+    await packager.setup();
+    await packager.start();
+    await packager.end();
+
+    expect(writeBundleLoadersSpy).toHaveBeenCalledTimes(1);
   });
 });
