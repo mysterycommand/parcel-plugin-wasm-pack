3bedaf070cdd546335cf4dfd415c0ce9254cf6d7
Matt Hayes <matt@mysterycommand.com>
2019-06-30 16:37:22 -0400

    test(WasmPackPackager): adds a fixture for the positive match case, tests generated outpu
    also cleans up the test a bit with a little bundle helper

--- src/WasmPackPackager.test.js
+++ src/WasmPackPackager.test.js
@@ -8,35 +8,56 @@ const registerPlugin = require('../');
 
 const readFile = promisify(fs.readFile);
 
-describe('WasmPackPackager', () => {
-  it('should not write a loader if this package has no wasm assets', async () => {
-    const writeBundleLoadersSpy = jest.spyOn(
-      Object.getPrototypeOf(WasmPackPackager.prototype),
-      'writeBundleLoaders',
-    );
+async function bundle(entryPath) {
+  const outDir = path.join(path.dirname(entryPath), '../dist');
+  const outFile = 'output.js';
 
+  const bundler = new Bundler(entryPath, {
+    outDir,
+    outFile,
+    hmr: false,
+    minify: false,
+    cache: false,
+    sourceMaps: false,
+  });
+
+  registerPlugin(bundler);
+  await bundler.bundle();
+
+  return path.join(outDir, outFile);
+}
+
+describe('WasmPackPackager', () => {
+  const writeBundleLoadersSpy = jest.spyOn(
+    Object.getPrototypeOf(WasmPackPackager.prototype),
+    'writeBundleLoaders',
+  );
+
+  beforeEach(() => {
+    writeBundleLoadersSpy.mockClear();
+  });
+
+  it('should not write a loader if this package has no wasm assets', async () => {
     const entryPath = require.resolve(
       './__fixtures__/without-wasm-assets/src/entry.js',
     );
 
-    const outDir = path.join(path.dirname(entryPath), '../dist');
-    const outFile = 'output.js';
-    const outPath = path.join(outDir, outFile);
-
-    const bundler = new Bundler(entryPath, {
-      outDir: path.dirname(outPath),
-      outFile: 'output.js',
-      hmr: false,
-      minify: false,
-      cache: false,
-      sourceMaps: false,
-    });
-    registerPlugin(bundler);
-
-    await bundler.bundle();
-    const outputStr = (await readFile(outPath)).toString();
+    const outputPath = await bundle(entryPath);
+    const outputStr = (await readFile(outputPath)).toString();
 
     expect(writeBundleLoadersSpy).toHaveBeenCalledTimes(1);
     expect(outputStr).not.toContain('function cacheReplace(id, mod) {');
   });
+
+  it('should write a loader if this package has wasm assets', async () => {
+    const entryPath = require.resolve(
+      './__fixtures__/with-wasm-assets/src/entry.js',
+    );
+
+    const outputPath = await bundle(entryPath);
+    const outputStr = (await readFile(outputPath)).toString();
+
+    expect(writeBundleLoadersSpy).toHaveBeenCalledTimes(0);
+    expect(outputStr).toContain('function cacheReplace(id, mod) {');
+  }, 20000);
 });
---
+++ src/__fixtures__/with-wasm-assets/src/entry.js
@@ -0,0 +1,2 @@
+import { run } from './lib.rs';
+run();
